66de4569606c8a0ba19692a6b02fe84422333e58
Fernando Saltoleto <fernandosaltoleto@hotmail.com>
2026-02-24 18:45:31 -0300

    ajustes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.3b91o2pmets"
+    "revision": "0.2rh7klshco8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
